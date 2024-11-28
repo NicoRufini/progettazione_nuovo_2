9edfc97af5092784f2ca977922bb4db2819ab24b
NicoRufini <nikorufini@gmail.com>
2024-11-28 19:24:53 +0100

    uiuiu

--- giancarlo/src/componenti/Navbar.jsx
+++ giancarlo/src/componenti/Navbar.jsx
@@ -1,10 +1,18 @@
 import './Navbar.css'
 import Link from './Link'
 
+const x = 3;
+const img = "vite";
+const imageStyle = {
+    height : x >= 3 ? "300px" : "1000000000px",
+    borderRadius : "90px"
+}
 
 function Navbar() {
     return (
         <>
+        <div id = 'box' className ='rotated rounded'>Ciao0o</div>
+        <img style = {imageStyle} src = {`/${img}.svg`} alt = "roba"/>
         <ul>
             <li><Link></Link></li>
             <li><Link></Link></li>

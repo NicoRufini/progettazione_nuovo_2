c26be267b70155509229493e2de751f424209271
NicoRufini <nikorufini@gmail.com>
2024-11-28 16:54:49 +0100

    hkvkhv

--- giancarlo/src/componenti/Navbar.jsx
+++ giancarlo/src/componenti/Navbar.jsx
@@ -1,8 +1,10 @@
 import './Navbar.css'
 import Link from './Link'
 
+
 function Navbar() {
     return (
+        <>
         <ul>
             <li><Link></Link></li>
             <li><Link></Link></li>
@@ -11,6 +13,7 @@ function Navbar() {
             <li><Link></Link></li>
             <li><Link></Link></li>
         </ul>
+        </>
     )
 }
 
